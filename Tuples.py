ff909aef01b087f1afcd541ab46ff526c5dda83a
Jake Brown <55098856+Brow0971@users.noreply.github.com>
2019-10-15 11:08:05 -0400

    Add files via upload
    Forgot to add the F1

--- Tuples.py
+++ Tuples.py
@@ -38,8 +38,41 @@ for row in pm:
 lcd.show()
 
 
+backlight.set_all(255, 0, 0)
+backlight.show()
+
+lcd.clear()
+lcd.show()
 
 
+f1 =  [
+[1,1,1,1,1,1,1,1],
+[1,1,1,1,1,1,1,1],
+[0,1,1,1,1,1,1,0],
+[1,0,1,1,1,1,0,1],
+[1,0,0,1,1,0,0,1],
+[1,0,0,1,1,0,0,1],
+[0,0,0,1,1,0,0,0],
+[0,0,0,0,0,0,0,0]
+]
 
 
+x=0
+y=0
+
+for row in f1:
+	y= y + 1
+
+	for state in row:
+		x = x + 1
+
+		lcd.set_pixel(x, y, state)
+	x=0
+lcd.show()
+
+backlight.set_all(255, 0, 0)
+backlight.show()
+
+lcd.clear()
+lcd.show()
 
